999da02063c58bb02a941a0ee2a3fe5dbd0f5427
Mimi <mimitraugott@gmail.com>
2019-10-02 17:49:54 -0600

    again

--- practice.js
+++ practice.js
@@ -16,10 +16,10 @@ var arr = [10,20,30];
   Create a function named 'first' that is given 'arr' as an argument.
   Return the first item in the given array.
 */
+function first(arr){ 
+  return arr[0]
 
-//Code Here
-
-
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -32,9 +32,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
-
-
+function last(arr){
+  return arr[2]
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -47,8 +47,12 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
-
+function looper(family){
+  for (let i = 0; i < family.length; i++){
+    alert(family[i])
+  }
+ console.log(family)
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -62,8 +66,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
-
+function reversedLooper(letters){
+  for (let i = letters.length-1; i >= 0; i--){
+    alert(letters[i])
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -76,14 +83,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Write a function named evenFinder that is given nums as it's only argument.
   Return an array that contains the even numbers from the nums array.
 */
-
-//Code Here
-
-
-
-
-
-
+let evens = []
+function evenFinder(nums){
+  for (let i = 0; i < nums.length; i++){
+    if (nums[i] % 2 != 1){
+      evens.push(nums[i])
+    }
+  }
+  return evens
+}
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
