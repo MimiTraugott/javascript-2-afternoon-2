548c075561ab290668b7882d30f482dd2b31e041
Mimi <mimitraugott@gmail.com>
2019-10-19 21:06:34 -0600

    practice push

--- practice.js
+++ practice.js
@@ -5,37 +5,29 @@
 
   You can refresh the page at any time to re-run all the tests.
 */
-
 ////////// PROBLEM 1 //////////
 
 // Do not edit the code below.
 var arr = [10,20,30];
 // Do not edit the code above.
-
 /*
   Create a function named 'first' that is given 'arr' as an argument.
   Return the first item in the given array.
 */
-function first(arr){ 
+function first (arr) {
   return arr[0]
-
 }
-
 ////////// PROBLEM 2 //////////
-
 // Do not edit the code below.
 var arr = [40,50,60];
 // Do not edit the code above.
-
 /*
   Create a function named 'last' that is given 'arr' as an argument. 
   Return the last item in the given array.
 */
-
-function last(arr){
+function last (arr) {
   return arr[2]
 }
-
 ////////// PROBLEM 3 //////////
 
 // Do not edit the code below.
@@ -46,43 +38,44 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Create a function named 'looper' that is given family as it's only argument. 
   Loop through the given array and alert every item in the array.
 */
-
-function looper(family){
-  for (let i = 0; i < family.length; i++){
-    alert(family[i])
-  }
- console.log(family)
+function looper (family) {
+  for (let i = 0; i < family.length; i++)
+  alert(family[i])
 }
-
+// console.log(looper)
 
 ////////// PROBLEM 4 //////////
-
 // Do not edit the code below.
 var letters = ['A', 'B', 'C', 'D', 'E'];
 // Do not edit the code above.
-
 /*
   Write a function called reversedLooper that is given letters as it's only argument. 
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
-
 function reversedLooper(letters){
   for (let i = letters.length-1; i >= 0; i--){
     alert(letters[i])
   }
 }
 
-
 ////////// PROBLEM 5 //////////
-
 // Do not edit the code below.
 var nums = [1,2,3,6,22,98,45,23,22,12];
 // Do not edit the code above.
-
 /*
   Write a function named evenFinder that is given nums as it's only argument.
   Return an array that contains the even numbers from the nums array.
 */
+let evens = []
+
+function evenFinder (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i]%2 != 1) {
+      evens.push(nums[i])
+    }
+  }
+  return evens
+}
 let evens = []
 function evenFinder(nums){
   for (let i = 0; i < nums.length; i++){
@@ -92,15 +85,8 @@ function evenFinder(nums){
   }
   return evens
 }
-
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
-
-
-
-
-
-
 
 ////////// PROBLEM 6 //////////
 
